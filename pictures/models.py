1018d814ec8835289254bfb20899a2893117279f
Ruben <rubenzurr@gmail.com>
2021-03-12 17:41:53 +0300

    Added parent_picture into model DB, also minor fixes

--- pictures/models.py
+++ pictures/models.py
@@ -7,6 +7,7 @@ import urllib.request
 
 # Create your models here.
 class Picture(models.Model):
+    parent_picture = models.ImageField(upload_to='images/parent_pictures', )
     picture_name = models.CharField(max_length=50, null=True)
     picture = models.ImageField(upload_to='images')
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
--- pictures/views.py
+++ pictures/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.core.files.base import ContentFile
 import requests
 import re
-from PIL import Image
+from PIL import Image, ImageOps
 import io
 
 # Create your views here.
@@ -18,7 +18,6 @@ class PicturesListView(View):
     template_name = 'pictures/picture_list.html'
 
     def get(self, request):
-        # https://www.pythonanywhere.com/user/Rubikon/files/home/Rubikon/django_projects/mysite/ads/templates/ads/ad_list.html?edit
         objects = Picture.objects.all()
         ctx = {'picture_list': objects}
         return render(request, self.template_name, ctx)
@@ -44,15 +43,38 @@ class PictureDetailView(View):
             ctx = {'form': form, 'picture': pic}
             return render(request, self.template_name, ctx)
 
-        image = Image.open(pic.picture)
-        image.thumbnail((form.cleaned_data['width'], form.cleaned_data['height']), Image.ANTIALIAS)
+        image = Image.open(pic.parent_picture)
+        image = image.convert('RGB') # Needed incase our picture has alpha/transparency
+        if form.cleaned_data['width'] and not form.cleaned_data['height']:
+            width = form.cleaned_data['width']
+            height = image.height
+        elif form.cleaned_data['height'] and not form.cleaned_data['width']:
+            height = form.cleaned_data['height']
+            width = image.width
+        else:
+            # The only issue is picture doesn't fit in this new given field
+            width = form.cleaned_data['width']
+            height = form.cleaned_data['height']
+
+        # Calculating ratio to resize image (works for both scale down and upper)
+        if width < image.width or height < image.height:
+            resize_ratio = min(width/image.width, height/image.height)
+        elif width > image.width or height > image.height:
+            resize_ratio = max(width/image.width, height/image.height)
+        else:
+            resize_ratio = 1
+
+        new_width = int(image.width*resize_ratio)
+        new_height = int(image.height*resize_ratio)
+        img_copy = image.resize((new_width, new_height), Image.ANTIALIAS)
+        img_fit = ImageOps.fit(img_copy, (1000, 1000), centering=(0.5, 0.5))
         filename, extension = pic.picture_name.split('.')
-        # image.save(f'{filename}_{form.cleaned_data["width"]}x{form.cleaned_data["height"]}.{extension}', quality=80)
         # Turning edited picture into byte array
         byte_img = io.BytesIO()
-        image.save(byte_img, format='PNG')
+        # img_copy.save(byte_img, format='JPEG')
+        img_fit.save(byte_img, format='JPEG')
         byte_img = byte_img.getvalue()
-        pic.picture = ContentFile(byte_img, name=f'resize/{filename}_{form.cleaned_data["width"]}x{form.cleaned_data["height"]}.{extension}')
+        pic.picture = ContentFile(byte_img, name=f'resize/{filename}/{filename}_{new_width}x{new_height}.{extension}')
         pic.save()
         context = {'form': form, 'picture': pic}
         return render(request, self.template_name, context)
@@ -68,6 +90,7 @@ class PictureCreateView(View):
         return render(request, self.template_name, ctx)
 
     def post(self, request, pk=None):
+        # Here we add new image to db
         form = UploadImageForm(request.POST, request.FILES or None)
 
         if not form.is_valid():
@@ -76,7 +99,8 @@ class PictureCreateView(View):
 
         pic = Picture()
         if request.FILES:
-            pic.picture = form.cleaned_data['picture']
+            pic.parent_picture = form.cleaned_data['picture']
+            pic.parent = form.cleaned_data['picture']
             for filename in request.FILES:
                 pic.picture_name = request.FILES[filename]
                 pic.content_type = request.FILES[filename].content_type
@@ -89,6 +113,7 @@ class PictureCreateView(View):
                     fname = re.findall('filename=(.+)', resp.headers["Content-Disposition"])[0]
                 else:
                     fname = url.split("/")[-1]
+                pic.parent_picture = ContentFile(resp.content, name=fname)
                 pic.picture = ContentFile(resp.content, name=fname)
                 pic.picture_name = fname
                 pic.content_type = resp.headers['Content-Type']
